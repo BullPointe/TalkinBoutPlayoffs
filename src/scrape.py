88bb6678a1007206e02c5598d75870d1ab7a6b7f
Sahil Jaganmohan <dev.sahil.jaganmohan@gmail.com>
2022-01-22 03:14:56 -0500

    Ugh new pages...

--- src/scrape.py
+++ src/scrape.py
@@ -1,9 +1,12 @@
 from multiprocessing.sharedctypes import Value
 from wsgiref import headers
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import pandas as pd
 import re
 import urllib.request
+import requests
+
 
 def is_num_or_float(value):
     try:
@@ -16,16 +19,22 @@ class Scraper:
     def __init__(self) -> None:
         self.status = 0
         self.scrape_url = None
-    def set_url(self,url: str):
+    def set_url(self,url: str,name: str):
         self.scrape_url = url
+        self.name = name
         print("URL Changed to:",self.scrape_url," Status:",self.status)
     def get_url(self) -> str:
         return self.scrape_url
     def get_status(self) -> int:
         return self.status
     def parse_url(self) -> None:
-        self.source = urllib.request.urlopen(self.scrape_url).read()
-        self.soup = BeautifulSoup(self.source,'html.parser')
+        # driver = webdriver.Chrome()
+        # driver.get(self.scrape_url)
+        # html = driver.page_source
+        self.source = urllib.request.urlopen(self.scrape_url)
+        r = requests.get(self.scrape_url)
+        print(r.text)
+        self.soup = BeautifulSoup(self)
         # print(self.soup)
     def parse_html(self) -> None:
         self.rows=list()
@@ -41,7 +50,7 @@ class Scraper:
                 self.rows.append(row)
 
         self.status = 1
-    def parse_stats(self):
+    def parse_stats(self, data):
         headers_list = list()
         stats_list = list()
         for col in self.header.findAll("th"):
@@ -60,9 +69,10 @@ class Scraper:
             #Add row
             stats_list.append(ind_stats)
         
-        
+        # fill dataframe
+        df_stats = pd.DataFrame(stats_list, columns=headers_list)
+        data.add_table(self.name,df_stats)
 
-        print(stats_list)
 
     def get_html_table_attr(self):
         if self.status > 0:
@@ -70,33 +80,35 @@ class Scraper:
         else:
             print("Please Parse Html")
 
-
+    
 
 
 class Player_Statistics:
 
     def __init__(self) -> None:
-        self.tables = []
+        self.tables = {}
+        self.table_IDS = ['Passing','Rushing','Receiving','Kicking','Kickoffs','Punting','Returns','Defense']
     
-    def add_table(name,df):
+    def add_table(self,name,df):
         self.tables[name] = df
     
-
+    def get_all_tables(self,scraper,base_url):
+        for i,name in enumerate(self.table_IDS):
+            url = base_url[0] + str(i) + base_url[1]
+            scraper.set_url(url, name)
+            scraper.parse_url()
+            # scraper.parse_html()
+            # scraper.parse_stats(self)
+            
 
 
 def main():
     sc1 = Scraper()
-    # pc1 = Player_Statistics()
-    sc1.set_url(
-        "https://sports.yahoo.com/nfl/stats/weekly/?sortStatId=PASSING_YARDS&selectedTable=0&week={%22week%22:19,%22seasonPhase%22:%22POSTSEASON%22}"
-        )
+    pc1 = Player_Statistics()
+    base_url = ["https://sports.yahoo.com/nfl/stats/weekly/?sortStatId=PASSING_YARDS&selectedTable=","&week={%22week%22:19,%22seasonPhase%22:%22POSTSEASON%22}"]
+    pc1.get_all_tables(sc1,base_url)
 
-    sc1.parse_url()
-    sc1.parse_html()
-    # print()
-    sc1.get_html_table_attr()
-    
-    sc1.parse_stats()
+    print(pc1.tables['Defense'])
 
 
 
