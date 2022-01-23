fb675d18dff9b8e4b0ac720d3b5909700e3f7b1d
krkral <kralk@purdue.edu>
2022-01-23 02:42:57 -0500

    Advanced website functionality

--- src/web/webpage.py
+++ src/web/webpage.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template      
+from flask import Flask, render_template, request, redirect, url_for    
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        if request.form.get('action1') == 'League Leaderboard':
+            return redirect(url_for('table'))
+        elif  request.form.get('action2') == 'Player Statistics':
+            return redirect(url_for('stats'))
+        else:
+            pass # unknown
+    elif request.method == 'GET':
+        return render_template('home.html')
     return render_template("home.html")
 
 @app.route("/table")
 def table():
     return render_template("table.html")
+
+@app.route("/stats")
+def stats():
+    return render_template("stats.html")
+    
     
 if __name__ == "__main__":
     app.run(debug=True)
